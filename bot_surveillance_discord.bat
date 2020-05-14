e3a20eb3cf0105c13f4650449439e442371b1095
Martial Tarizzo <martial.tarizzo@gmail.com>
2020-05-14 19:31:32 +0200

    Update bot_surveillance_discord.bat caractère @ devant les commentaires pour éviter la sortie sur la console

--- bot_surveillance_discord.bat
+++ bot_surveillance_discord.bat
@@ -1,8 +1,8 @@
-REM Pour Windows - testé sous W10
-REM Fichier de commande pour lancer le script de surveillance
+@REM Pour Windows - testé sous W10
+@REM Fichier de commande pour lancer le script de surveillance
 
-REM Définition de l'envrironnement virtuel, inclusion dans le path
-path = %CD%\venv\Scripts;%path%
+@REM Définition de l'envrironnement virtuel, inclusion dans le path
+@path = %CD%\venv\Scripts;%path%
 
 REM Lancement du bot ...
 python bot_surveillance_discord.py
